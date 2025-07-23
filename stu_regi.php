68ac3110bce315291cb68baaff1fd51fef4b8208
junel0026 <junelsinmoe@gmail.com>
2025-07-23 23:44:36 +0630

    J second time

--- stu_regi.php
+++ stu_regi.php
@@ -4,7 +4,22 @@ echo "<h1> Student Registration Form </h1>";
 $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
 $Fname = $_POST["Fname"];
+$mobileph = $_POST["mobileph"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$choice = $_POST["choice"];
+$department = $_POST["department"];
+$city = $_POST["$city"];
+$address = $_POST["address"];
+
 echo "First Name = $firstname -
 Last Name = $lastname <br>
-Father's name = $Fname <br>";
+Father's name = $Fname <br>
+Mobile Phone Number = $mobileph <br>
+Email = $email <br>
+Password = $password <br>
+Gender = $choice<br>
+Department = $department<br>
+City = $city<br>
+Address = $address<br>";
 ?>

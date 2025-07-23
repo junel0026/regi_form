3102636910fd7b399ac18d7d8040ed46ae8879a0
junel0026 <junelsinmoe@gmail.com>
2025-07-23 22:13:57 +0630

    first time committed by Junel

---
+++ stu_regi.php
@@ -0,0 +1,10 @@
+
+<?php
+echo "<h1> Student Registration Form </h1>";
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$Fname = $_POST["Fname"];
+echo "First Name = $firstname -
+Last Name = $lastname <br>
+Father's name = $Fname <br>";
+?>

88a249bd39caa110839844373183d58344c260ea
junel0026 <junelsinmoe@gmail.com>
2025-07-25 10:17:10 +0630

    error

---
+++ form.php
@@ -0,0 +1,87 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <style>
+    .error {
+      color: red;
+      font-size: 14px;
+    }
+  </style>
+</head>
+<body>
+
+<h1> Student Registration Form </h1>
+<form action="stu_regi.php" method="post">
+  First Name:<br>
+  <input type="text" name="firstname">
+  <span class="error">
+    <?php
+    if (isset($_GET['error']) && $_GET['error'] == 'firstname') {
+        echo "First name is required";
+    }
+    ?>
+  </span>
+  <input type="text" name="lastname" placeholder="Last Name" ><br><br>
+    <span class="error">
+    <?php
+    if (isset($_GET['error']) && $_GET['error'] == 'lastname') {
+        echo "Last name is required";
+    }
+    ?>
+    </span><br><br>
+
+
+  <label>Father's Name :</label>
+  <input type="text" name="father_name"><br><br>
+
+  <label>Date of Birth :</label>
+  <input type="text" name="dob_day" placeholder="Day">
+  <input type="text" name="dob_month" placeholder="Month">
+  <input type="text" name="dob_year" placeholder="Year"><br><br>
+
+  <label>Mobile Number : +95 -</label>
+  <input type="text" name="mobile"><br><br>
+
+  <label>Email :</label>
+  <input type="email" name="email" placeholder="name23@gmail.com"><br><br>
+
+  Password:
+  <input type="text" name="password"><br><br>
+
+  Gender:<br>
+  <input type="radio" id="yes" name="choice" value="Male">
+  <label for="yes">Male</label>
+
+  <input type="radio" id="no" name="choice" value="Female">
+  <label for="no">Female</label><br><br>
+
+  Department:<br>
+  <input type="radio" id="one" name="department" value="English">
+  <label for="one">English</label>
+
+  <input type="radio" id="two" name="department" value="Computer">
+  <label for="two">Computer</label>
+
+  <input type="radio" id="three" name="department" value="Business">
+  <label for="three">Business</label><br><br>
+
+  <label for="course">Course :</label>
+  <select name="course" id="course">
+    <option value="Choose">Choose</option>
+    <option value="Social">Social Studies</option>
+    <option value="Computer">Computer Studies</option>
+    <option value="English">English</option>
+    <option value="Music">Music</option>
+  </select><br><br>
+
+  City:
+  <input type="text" name="city"><br><br>
+
+  Address:<br>
+  <textarea name="address" id="address" rows="4" cols="50"></textarea><br><br>
+
+  <input type="submit" value="Register">
+</form>
+
+</body>
+</html>
--- stu_regi.php
+++ stu_regi.php
@@ -1,21 +1,38 @@
-
 <?php
-echo "<h1> Student Registration Form </h1>";
-$firstname = $_POST["firstname"];
-$lastname = $_POST["lastname"];
-$Fname = $_POST["Fname"];
-$mobileph = $_POST["mobileph"];
-$email = $_POST["email"];
-$password = $_POST["password"];
-$choice = $_POST["choice"];
-$department = $_POST["department"];
-$city = $_POST["$city"];
-$address = $_POST["address"];
+$firstname = $_POST['firstname'] ?? '';
 
-echo "First Name = $firstname -
-Last Name = $lastname <br>
-Father's name = $Fname <br>
-Mobile Phone Number = $mobileph <br>
+if (empty($firstname)) {
+    // Redirect back with error
+    header("Location: form.php?error=firstname");
+    exit();
+}
+
+// Collect all values (no error checking for other fields here)
+$lastname = $_POST["lastname"] ?? '';
+
+if (empty($lastname)) {
+    // Redirect back with error
+    header("Location: form.php?error=lastname");
+    exit();
+}
+
+$father_name = $_POST["father_name"] ?? '';
+$mobile = $_POST["mobile"] ?? '';
+$dob_day = $_POST["dob_day"] ?? '';
+$dob_month = $_POST["dob_month"] ?? '';
+$dob_year = $_POST["dob_year"] ?? '';
+$email = $_POST["email"] ?? '';
+$password = $_POST["password"] ?? '';
+$choice = $_POST["choice"] ?? '';
+$department = $_POST["department"] ?? '';
+$city = $_POST["city"] ?? '';
+$address = $_POST["address"] ?? '';
+
+echo "<h1>Submitted Registration Details</h1>";
+echo "First Name = $firstname - Last Name = $lastname <br>
+Father's name = $father_name <br>
+Date of Birth = $dob_day-$dob_month-$dob_year <br>
+Mobile Phone Number = $mobile <br>
 Email = $email <br>
 Password = $password <br>
 Gender = $choice<br>
